3814120f946241dc3badde78ad776735a9c59557
santosjennifer <asantos.jennifer@gmail.com>
2024-04-10 22:50:46 -0300

    Adicionado gateway-server

--- gateway-server/src/main/java/com/petshop/Application.java
+++ gateway-server/src/main/java/com/petshop/Application.java
@@ -2,10 +2,8 @@ package com.petshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class Application {
 
 	public static void main(String[] args) {
